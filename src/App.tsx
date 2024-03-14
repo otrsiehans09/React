cf026147b1183f55aa5da4ce7335b9eab7f5cde0
otrsiehans09 <hansouatt09@gmail.com>
2024-03-14 20:00:22 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import RHOBSChallengeInfo from "./RHOBSChallengeInfo";
+
+const App = () => {
+  return (
+    <div className="App">
+      <RHOBSChallengeInfo />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/RHOBSChallengeInfo.js
@@ -0,0 +1,18 @@
+// RHOBSChallengeInfo.js
+import React from "react";
+
+const RHOBSChallengeInfo = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1 style={{ fontSize: "3rem", marginBottom: "20px" }}>
+        RHOBS Challenge
+      </h1>
+      <h2 style={{ fontSize: "2rem", marginBottom: "20px" }}>Is Planet</h2>
+      <h3 style={{ fontSize: "1.5rem", marginBottom: "20px" }}>Gravity</h3>
+      <h4 style={{ fontSize: "1.2rem", marginBottom: "20px" }}>Bodies:</h4>
+      <p style={{ fontSize: "1rem" }}>Info on the body</p>
+    </div>
+  );
+};
+
+export default RHOBSChallengeInfo;
